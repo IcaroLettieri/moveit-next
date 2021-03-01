6ca4775800b73eba09208d478faa1726331413be
icarolettieri <icaro_lettieri@live.com>
2021-02-28 23:21:25 -0300

    chore: implementação do provider theme e login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
+import { LoginProvider } from '../contexts/LoginContext';
+import { ThemeProvider } from '../contexts/ThemeContext';
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <LoginProvider>
+        <Component {...pageProps} />
+      </LoginProvider>
+    </ThemeProvider>
+  );
 }
 
 export default MyApp
